adcea5c9fe07a17e6a4fb39820db3610ac3fe3c3
pwierdak <paulinawierdak95@gmail.com>
2022-09-21 20:49:41 +0200

    addition, subtraction, multiplication, division added

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public int addition(int x, int y){
+        int z=x+y;
+        return z;
+    }
+    public int substraction(int x, int y){
+        return x-y;
+    }
+    public int multiplication(int x, int y){
+        return x*y;
+    }
+    public int division(int x, int y){
+        return x/y;
+    }
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! What do you want to do?\nSelect:\n1.addition\n2.substraction\n3.multiplication\n4.division");
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Please, enter first value.");
-        float x= scanner.nextFloat();
-        System.out.println("Please, enter second value.");
-        float y= scanner.nextFloat();
-        int select= scanner.nextInt();
-        switch (select){
-            case 1:
-                System.out.println("result " + (x+y));
-                break;
-            case 2:
-                System.out.println("result " + (x-y));
-                break;
-            case 3:
-                System.out.println("result " + (x*y));
-                break;
-            case 4:
-                System.out.println("result " + (x/y));
-
-        }
+        Calculator cal=new Calculator();
+        cal.addition(7,1);
+        System.out.println(cal.addition(7,1));
+        System.out.println(cal.substraction(6,5));
+        System.out.println(cal.multiplication(5, 6));
+        System.out.println(cal.division(30, 5));
     }
 }
+
